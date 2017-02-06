b4d3767df880de028d725e8fe6e12d48cec15eff
monroema <monroema@oregonstate.edu>
2017-02-05 22:32:25 -0700

    fixed it

--- main.py
+++ main.py
@@ -54,6 +54,7 @@ class CustomersHandler(webapp2.RequestHandler):
             for entity in customer_list:
                 single_customer = entity.to_dict()
                 single_customer['id'] = entity.key.id()
+                single_customer['self'] = '/customers/' +entity.key.id()
                 results.append(single_customer)
             self.response.write(json.dumps(results))
 
